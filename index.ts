68ff8d8962082e0cfba9c61e41269ad7eed682fb
bossoncode <akinkunmiisrael41@gmail.com>
2023-03-22 00:55:47 +0100

    added count route

--- index.ts
+++ index.ts
@@ -5,6 +5,7 @@ import get from "./controllers/get";
 import create from "./controllers/create";
 import decrypt from "./controllers/decrypt";
 import dotenv from "dotenv";
+import count_view from "./controllers/count_view";
 
 dotenv.config();
 
@@ -19,6 +20,7 @@ app.get("/", async (req: Request, res: Response) => {
 
 app.post("/create", create);
 app.get("/decrypt", decrypt);
+app.post("/count", count_view);
 app.get("/:code", get);
 
 app.listen(process.env.PORT, () => console.log("Server is live"));
