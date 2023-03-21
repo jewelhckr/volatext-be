952c5033c8e61b2cd9c3f4fe57d08c5fbca9fd7f
bossoncode <akinkunmiisrael41@gmail.com>
2023-03-22 00:54:44 +0100

    create count_view function

---
+++ controllers/count_view.ts
@@ -0,0 +1,12 @@
+import dayjs from "dayjs";
+import { Request, Response } from "express";
+import prisma from "../prisma/prisma";
+
+const count_view = async (req: Request, res: Response) => {
+  prisma.viewsCount.create({
+    data: {
+      created: dayjs().toDate(),
+      sharing_code: req.query.code as string,
+    },
+  });
+};
