81b546e33393f3a4a62b040c1c99a4ac1709cb48
bossoncode <akinkunmiisrael41@gmail.com>
2023-03-22 09:23:06 +0100

    @types

--- controllers/get.ts
+++ controllers/get.ts
@@ -4,8 +4,8 @@ import prisma from "../prisma/prisma";
 import cryptr from "../utils/cryptr";
 
 export default async function get(req: Request, res: Response) {
-  const code: any = req.params.code;
-
+  const code: string = req.params.code as string;
+  const accessToken: string = req.query.accessToken as string;
   const text = await prisma.text.findUnique({
     where: {
       sharing_code: code,
@@ -26,6 +26,29 @@ export default async function get(req: Request, res: Response) {
       where: { sharing_code: code },
     });
     return res.status(404).json({ message: "Text not found" });
+  }
+
+  const viewsCount = async () => {
+    return await prisma.viewsCount.count({
+      where: {
+        sharing_code: code,
+      },
+    });
+  };
+
+  const isOwner = async () => {
+    return text.accessToken === accessToken;
+  };
+
+  if (await isOwner()) {
+    return res.status(200).json({
+      text: cryptr.decrypt(text.text),
+      sharing_code: text.sharing_code,
+      diff,
+      isProtected: false,
+      viewsCount: text.viewsCount,
+      views: await viewsCount(),
+    });
   }
 
   if (text.isProtected) {
